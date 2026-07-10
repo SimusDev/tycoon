77fc3df9841bbf1aa97e7f2033cd1f197e5515b2
zxcvlad78 <danyaps@mail.ru>
2026-07-11 00:10:51 +0300

    sa

--- src/game/scripts/MultiplayerSpawner.cs
+++ src/game/scripts/MultiplayerSpawner.cs
@@ -1,1 +1,67 @@
+using System.Threading.Tasks;
 using Godot;
+
+[GlobalClass]
+public partial class CustomMultiplayerSpawner : MultiplayerSpawner
+{
+    [Export]
+    public PackedScene PlayerPrefab;
+    [Export]
+    public Godot.Collections.Array<Node3D> SpawnPoints;
+
+    public override void _Ready()
+    {
+        if (Multiplayer.IsServer())
+        {
+            Multiplayer.PeerConnected += onPeerConnected;
+        }
+    }
+
+    private void onPeerConnected(long id)
+    {
+        spawnPlayer(id);
+    }
+
+    private void spawnPlayer(long id)
+    {
+        try
+        {
+            Node spawnNode = GetNodeOrNull(SpawnPath);
+            Node playerInstance = PlayerPrefab.Instantiate();
+            playerInstance.Name = id.ToString();
+            
+
+        }
+        catch
+        {
+            // sas
+        }
+    }
+
+    private void onPlayerInstanceTreeEntered(long id)
+    {
+        Rpc("initSpawnedPlayer", id);
+    }
+
+
+    private async Task<Error> initSpawnedPlayer(long id)
+    {
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+
+        if (SpawnPoints.Count == 0)
+        {
+            return Error.Failed;
+        }
+
+        Node spawnNode = GetNodeOrNull(SpawnPath);
+        if (!IsInstanceValid(spawnNode)) { return Error.Failed; }
+
+        Node3D playerNode = spawnNode.GetNodeOrNull<Node3D>(id.ToString());
+        if (!IsInstanceValid(playerNode)) { return Error.Failed; }
+        
+        playerNode.GlobalPosition = SpawnPoints.PickRandom().GlobalPosition;
+
+        return Error.Ok;
+    }
+    
+}
