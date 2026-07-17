9816e17322833163768809996684c364a14630c7
SimusDev <simusdev@mail.ru>
2026-07-17 17:26:26 +0300

    Update Inventory.cs

--- src/game/scripts/inventory/Inventory.cs
+++ src/game/scripts/inventory/Inventory.cs
@@ -8,10 +8,24 @@ public partial class Inventory : Node
 	private GDNetStream _stream = new();
 	private List<InventorySlot> _slots;
 
+	private GDNetRpc _rpc = new();
+
 	public override void _Ready()
 	{
+		_rpc.BindOwnerAsNode(this);
+
 		if (!Multiplayer.IsServer())
+		{
 			RpcId(GameServer.ServerId, MethodName.SyncToSender);
+			_rpc.InvokeOnServer(nameof(RpcTest));
+		}
+			
+	}
+
+	[GDNetRpc(permission:Permission.Any)]
+	public void RpcTest()
+	{
+		GD.Print($"hello from {_rpc.GetRemoteSender()}");
 	}
 
 	[Rpc(mode: MultiplayerApi.RpcMode.AnyPeer, TransferChannel = (int)GameServer.TransferChannels.Inventory)]
